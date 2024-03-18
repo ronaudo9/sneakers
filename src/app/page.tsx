34b4bb68c022eda78d1942d4cc8f80ae4441c0df
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2024-03-18 10:52:04 +0900

    ベースデザイン（レスポンシブ対応）の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
 export default function Home() {
   return (
     <>
-      <header className="h-20 w-full bg-gray-800">Home</header>
+      <header className="fixed top-0 h-20 w-full bg-gray-800 ">Sneakers</header>
+      <div className="mb-24 h-150 w-full   bg-yellow-100">video</div>
+      <div className="mb-24">
+        <div
+          className="mb-7 h-14
+         bg-green-50 text-center text-4xl"
+        >
+          PICK UP
+        </div>
+        <div className="h-64 w-full bg-blue-50">picture</div>
+      </div>
+      <div className="mx-auto max-w-6xl px-4">
+        <div className="mb-14">
+          <div className="mb-7 h-14  bg-slate-100 text-center text-4xl">
+            FEATURE
+          </div>
+          <div className=" grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
+            <div className="h-106 bg-gray-200">aa</div>
+            <div className="h-106 bg-gray-200">a</div>
+            <div className="h-106 bg-gray-200">cc</div>
+            <div className="h-106 bg-gray-200">cc</div>
+            <div className="h-106 bg-gray-200">cc</div>
+            <div className="h-106 bg-gray-200">cc</div>
+            <div className="h-106 bg-gray-200">cc</div>
+            <div className="h-106 bg-gray-200">cc</div>
+            <div className="h-106 bg-gray-200">cc</div>
+          </div>
+        </div>
+      </div>
+      <footer className="w-full bg-gray-500">
+        <div className="mx-auto  max-w-6xl px-4">
+          <div className="pt-12">
+            <div className=" mb-7 text-center text-4xl">CONTACT</div>
+            <div className="flex flex-col md:flex-row md:justify-between">
+              <div className="mb-7 bg-green-50 md:w-45">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </div>
+              <div className="bg-blue-50 md:w-45">bb</div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
