00cf6a4b6d3d59f1ad0cc68f2ae083e25c9dad35
Naoyuki Kojima <116235104+ronaudo9@users.noreply.github.com>
2024-03-25 15:25:46 +0900

    featureのデザイン完了 (#7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
-import { Pickup } from "./components/Pickup";
-import { Video } from "./components/Video";
+import { Feature } from "@/components/Feature";
+import { Pickup } from "@/components/Pickup";
+import { Video } from "@/components/Video";
 
 export default function Home() {
   return (
     <>
       <Video />
       <Pickup />
-      <div className="mx-auto max-w-6xl px-4 ">
-        <div id="feature" className="mb-14 mt-[-80px] pt-20 ">
-          <div className="mb-7 h-14  bg-slate-100 text-center text-4xl font-bold italic">
-            FEATURE
-          </div>
-          <div className=" grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
-            <div className="h-106 bg-gray-200">aa</div>
-            <div className="h-106 bg-gray-200">a</div>
-            <div className="h-106 bg-gray-200">cc</div>
-            <div className="h-106 bg-gray-200">cc</div>
-            <div className="h-106 bg-gray-200">cc</div>
-            <div className="h-106 bg-gray-200">cc</div>
-            <div className="h-106 bg-gray-200">cc</div>
-            <div className="h-106 bg-gray-200">cc</div>
-            <div className="h-106 bg-gray-200">cc</div>
-          </div>
-        </div>
-      </div>
+      <Feature />
       <footer id="contact" className=" w-full bg-gray-500 ">
         <div className="mx-auto   max-w-6xl  px-4 ">
           <div className="pt-12 ">
